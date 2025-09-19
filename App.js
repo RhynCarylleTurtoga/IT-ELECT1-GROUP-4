41dda1a68a02e30c6ae37702ae38b2cc10ee6dba
rincasm/github_username <rhynturtoga498@gmail.com>
2025-09-20 01:39:51 +0800

    CommentSection and ChatBot App

--- App.js
+++ App.js
@@ -1,14 +1,27 @@
-import React from 'react';
-import { SafeAreaView } from 'react-native';
-import CounterApp from './counter'; 
-import ColorChangerApp from './ColorChangerApp';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import Comment from './Comment';
+import ChatApp from './ChatApp';
 
 
 export default function App() {
+  const [bgColor, setBgColor] = useState('white');
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <CounterApp />
-      <ColorChangerApp />
-    </SafeAreaView>
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      <ChatApp/>
+      <Comment/>
+      <StatusBar style="auto" />
+    </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 10,
+  },
+});
